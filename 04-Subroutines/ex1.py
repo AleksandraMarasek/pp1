2a9844c71c4421d51b38c980923f95cf27042528
AleksandraMarasek <146991516+AleksandraMarasek@users.noreply.github.com>
2023-10-24 23:17:37 +0200

    Add files via upload

---
+++ 04-Subroutines/ex1.py
@@ -0,0 +1,23 @@
+a=len('computer science')
+
+print(f'lenght of the phrase"computer science": {a}')
+
+b=input('Enter a random letter')
+if len(b)==1 :
+    print(f'letter read from the keyboard: b')
+else:
+    print('Invalid input. please enter a singular letter')
+
+c_string=5068
+c_int=int(c_string)
+print(f'numeric representation of the string "5068": {c_int}')
+
+d_numeric = "20303"
+d_value = float(d_numeric)
+print(f"Numeric representation of the string '20303': {d_value}")
+
+e=[4,7,2,3,9,8]
+
+minimum=min(e)
+
+print(f'the smallest number given: {minimum}')
---
+++ 04-Subroutines/ex2.py
@@ -0,0 +1,15 @@
+import math
+
+log_5=math.log5
+print(f'natural log of 5: {log_5}')
+
+e_power_3=math.exp(3)
+print(f'e raised to the power of 3: {e_power_3}')
+
+square_2=math.sqrt(7)
+print(f'square root of 7: {square_2}')
+
+degrees=90
+radians=math.radians(degrees)
+sine=math.sin(radians)
+print(f'sine of 90 degrees: {sine}')
